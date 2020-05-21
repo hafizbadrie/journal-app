634ed6b6781a9588b5125eab9adc87d8053caf20
Hafiz Badrie Lubis <hafizbadrielubis@Hafiz-Badrie-MacBook-Pro.local>
2020-05-21 13:43:30 +0700

    Add basic auth capability and error interception

--- main.go
+++ main.go
@@ -14,12 +14,39 @@ const (
   port   = 5432
   user   = "postgres"
   dbname = "redash"
+  httpUsername = "hafizbadrie"
+  httpPassword = "1234567890"
 )
 
-func Index(writer http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-  fmt.Fprint(writer, "Hello World!")
+type HandleWithError func(http.ResponseWriter, *http.Request, httprouter.Params) error
+type errorAuth struct {
+  msg string
 }
 
+func (e *errorAuth) Error() string {
+  return e.msg
+}
+
+func HTTP(handle HandleWithError) httprouter.Handle {
+    return func(writer http.ResponseWriter, req *http.Request, params httprouter.Params) {
+      handle(writer, req, params)
+    }
+}
+
+func Index(writer http.ResponseWriter, req *http.Request, _ httprouter.Params) error {
+  username, password, _ := req.BasicAuth()
+
+  if username != httpUsername || password != httpPassword {
+    http.Error(writer, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+    return &errorAuth{"Unauthorized Access"}
+  }
+
+  fmt.Fprint(writer, "Hello World!")
+
+  return nil
+}
+
+// This is a sample function to add unit test
 func Sum(x int, y int) int {
   return x + y
 }
@@ -35,7 +62,7 @@ func main() {
 
   fmt.Println("Preparing webserver to run on :8080...")
   router := httprouter.New()
-  router.GET("/", Index)
+  router.GET("/", HTTP(Index))
 
   http.ListenAndServe(":8080", router)
 }
