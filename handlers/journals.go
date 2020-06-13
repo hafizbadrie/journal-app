4aa8278fc4f594b381e6c40192c406226a22c2eb
Hafiz Badrie Lubis <hafizbadrielubis@Hafiz-Badrie-MacBook-Pro.local>
2020-06-13 13:51:42 +0700

    [WIP] Cleanup and add more comments

--- handlers/journals.go
+++ handlers/journals.go
@@ -33,12 +33,15 @@ type JournalData struct {
 }
 
 func Journals(writer http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+  // Create connection
   psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbName)
   db, err := sql.Open("postgres", psqlInfo)
+  defer db.Close()
   if err != nil {
     panic(err)
   }
-  defer db.Close()
+
+  // Getting data and iterating the data
   sqlStatement := "SELECT * FROM journals"
   rows, err := db.Query(sqlStatement)
   if err != nil {
@@ -57,14 +60,16 @@ func Journals(writer http.ResponseWriter, req *http.Request, _ httprouter.Params
     data = append(data, journalData)
   }
 
+  // Preparing the response
   jsonData := JournalResponse{
     data,
     "OK",
   }
-
   response, err := json.Marshal(jsonData)
   if err != nil {
     panic(err)
   }
+
+  // Write the response out
   fmt.Fprint(writer, string(response))
 }
--- main.go
+++ main.go
@@ -8,52 +8,6 @@ import (
   handlers "github.com/hafizbadrie/journal-app/handlers"
 )
 
-// TESTING: json and functions
-type EmployeeData struct {
-  Data []Employee `json:"data"`
-  Status string `json:"status"`
-}
-
-type Employee struct {
-  FirstName string `json:"first_name"`
-}
-
-func (emp Employee) getFirstName() string {
-  return emp.FirstName
-}
-
-func (emp *Employee) changeFirstName(firstName string) {
-  emp.FirstName = firstName
-}
-
-func Show(writer http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-  var employees []Employee
-
-  empHafiz := Employee{
-    FirstName: "Hafiz",
-  }
-  empBadrie := Employee{
-    FirstName: "Badrie",
-  }
-  empLubis := Employee{
-    FirstName: "Lubis",
-  }
-  employees = append(employees, empHafiz)
-  employees = append(employees, empBadrie, empLubis)
-  employeeData := EmployeeData{
-    Data: employees,
-    Status: "OK",
-  }
-  var jsonData []byte
-  jsonData, err := json.Marshal(employeeData)
-  if err != nil  {
-    panic(err)
-  }
-
-  fmt.Println(string(jsonData))
-  fmt.Fprint(writer, string(jsonData))
-}
-
 func Index(writer http.ResponseWriter, req *http.Request, _ httprouter.Params) {
   //username, password, _ := req.BasicAuth()
 
